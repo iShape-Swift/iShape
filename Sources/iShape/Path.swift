dbdff0f6a44d486bcad2850b7ea9de353d2d6538
Nail Sharipov <nailxsharipov@gmail.com>
2023-07-10 10:27:50 +0300

    clean

---
+++ Sources/iShape/Path.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nail Sharipov on 10.07.2023.
+//
+
+import Foundation
--- Sources/iShape/Trim/IntShape+Trim.swift
+++
@@ -1,8 +0,0 @@
-//
-//  IntShape+Trim.swift
-//  
-//
-//  Created by Nail Sharipov on 14.03.2023.
-//
-
-import Foundation
