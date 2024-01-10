981c6e6400a8ce87301ec12b835f21273704835b
Nail Sharipov <nailxsharipov@gmail.com>
2024-01-10 16:00:57 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -1,24 +1,18 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "iShape",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "iShape",
             targets: ["iShape"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/iShape-Swift/iFixFloat", from: "1.1.0")
-       // .package(path: "../iFixFloat"),  // Local path to iFixFloat
+        .package(url: "https://github.com/iShape-Swift/iFixFloat", from: "1.2.0")
+//         .package(path: "../iFixFloat"),  // Local path to iFixFloat
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iShape",
             dependencies: ["iFixFloat"]),
